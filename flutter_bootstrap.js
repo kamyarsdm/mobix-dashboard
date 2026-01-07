3527539f8ccd2c67deee68ec3d6eb9a0f78e4857
home <home@kamyars-MacBook-Pro.local>
2026-01-08 03:09:15 +0330

    Fix base href for GitHub Pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "312493392"
+    serviceWorkerVersion: "3086490254"
   }
 });
